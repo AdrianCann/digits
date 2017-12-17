b4d3517c918d8539f1985959485f44c5d34019c3
Adrian Cann <adriantcann@gmail.com>
2017-12-17 11:25:06 -0500

    Scikit-learn SVM parameters and handwritten digits
    ***Handwritten Digits***
    * This dataset is a dataset that ships with scikit-learn. The
    handwritten digits datasets was transformed into
    "8x8 images of integer pixels in the range 0..16." More about it
    described here:
    http://scikit-learn.org/stable/datasets/index.html#optical-recognition-of-handwritten-digits-data-set
    * Since this data is already normalized, we do not need to perform
    feature scaling or anything like that.
    
    ***SVM***
    * Support Vector Machines
    * Default kernel="rbf" = radial basis function = Gaussian kernel
    
    ***Shuffling***
    * We are dividing the data into a few groups so we would like to ensure
    the order is random so that our groups are not bias
    
    ***Accuracy Score***
    * Percentage of correct predictions. This assesses how well the model
    predicts data in the test or cross validation set. However, this scoring
    becomes invalid if the data set were to be skewed towards a certain
    classification. For example, if most of the digits were 2s.
    
    ***Training, Cross Validation, Test***
    * The training set is used to actually train the model
    * The cross validation set is used to tweak parameters (in the case of
    Gaussian kernel SVM in scikit learn we tweak gamma and C)
    * The test set tells us how accurate our model is

---
+++ digits.py
@@ -0,0 +1,51 @@
+import sklearn
+print('The scikit-learn version is {}.'.format(sklearn.__version__))
+import numpy
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn import svm
+
+digits = datasets.load_digits()
+print("Digit information")
+print(type(digits.data[1]))
+print(digits.data[1])
+print(digits.data[1].size)
+
+m = len(digits.data)
+print('m = ', m, ' digits total')
+
+# https://stackoverflow.com/questions/4601373/better-way-to-shuffle-two-numpy-arrays-in-unison
+# Although apparently sklearn ahs a ShffleSplit as well
+# from sklearn.model_selection import ShuffleSplit
+def shuffle_in_unison(a, b):
+    rng_state = numpy.random.get_state()
+    numpy.random.shuffle(a)
+    numpy.random.set_state(rng_state)
+    numpy.random.shuffle(b)
+
+print("First digits: ", digits.target[:5])
+shuffle_in_unison(digits.data, digits.target)
+print("First digits: ", digits.target[:5])
+
+first_index = int(m * 0.6)
+training_set_x = digits.data[0:first_index]
+training_set_y = digits.target[0:first_index]
+second_index = int(m * 0.8)
+cross_validation_set_x = digits.data[first_index:second_index]
+cross_validation_set_y = digits.target[first_index:second_index]
+test_set = digits.data[second_index:]
+
+print("training_set length: ", len(training_set_x))
+print("cross_validation_set_x length: ", len(cross_validation_set_x))
+print("test_set length: ", len(test_set))
+
+clf = svm.SVC(gamma=0.001, C=100)
+
+#import numpy
+from sklearn.metrics import accuracy_score
+
+clf.fit(training_set_x, training_set_y)
+cross_validation_predictions = clf.predict(cross_validation_set_x)
+score = accuracy_score(cross_validation_predictions, cross_validation_set_y)
+print("Accuracy Score: ", score) # percent correct

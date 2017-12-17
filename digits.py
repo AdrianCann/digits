ad84219b29d93271dc03df8aaaef0c66b07021d1
Adrian Cann <adriantcann@gmail.com>
2017-12-17 17:51:51 -0500

    Use print statement better
    * Currently prints:
    
    The scikit-learn version is 0.18.1.
    Digit information
    Digit data is  <type 'numpy.ndarray'>
    digits.data[1]
    [  0.   0.   0.  12.  13.   5.   0.   0.   0.   0.   0.  11.  16.   9.
    0.
       0.   0.   0.   3.  15.  16.   6.   0.   0.   0.   7.  15.  16.  16.
    2.
       0.   0.   0.   0.   1.  16.  16.   3.   0.   0.   0.   0.   1.  16.
    16.
       6.   0.   0.   0.   0.   1.  16.  16.   6.   0.   0.   0.   0.   0.
    11.
      16.  10.   0.   0.]
    Size of data[1]: 64
    m = 1797 digits total
    First digits: [0 1 2 3 4]
    First digits: [2 3 2 6 6]
    training_set length: 1078
    cross_validation_set_x length: 359
    test_set length: 360
    Accuracy Score: 0.983286908078

--- digits.py
+++ digits.py
@@ -6,27 +6,31 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn import svm
 
+from sklearn.metrics import accuracy_score
+
 digits = datasets.load_digits()
 print("Digit information")
-print(type(digits.data[1]))
+print("Digit data is  " + str(type(digits.data[1])))
+print("digits.data[1]")
 print(digits.data[1])
-print(digits.data[1].size)
+print("Size of data[1]: " + str(digits.data[1].size))
 
 m = len(digits.data)
-print('m = ', m, ' digits total')
+print('m = ' + str(m) + ' digits total')
 
 # https://stackoverflow.com/questions/4601373/better-way-to-shuffle-two-numpy-arrays-in-unison
 # Although apparently sklearn ahs a ShffleSplit as well
 # from sklearn.model_selection import ShuffleSplit
+
 def shuffle_in_unison(a, b):
     rng_state = numpy.random.get_state()
     numpy.random.shuffle(a)
     numpy.random.set_state(rng_state)
     numpy.random.shuffle(b)
 
-print("First digits: ", digits.target[:5])
+print("First digits: " + str(digits.target[:5]))
 shuffle_in_unison(digits.data, digits.target)
-print("First digits: ", digits.target[:5])
+print("First digits: " + str(digits.target[:5]))
 
 first_index = int(m * 0.6)
 training_set_x = digits.data[0:first_index]
@@ -36,16 +40,13 @@ cross_validation_set_x = digits.data[first_index:second_index]
 cross_validation_set_y = digits.target[first_index:second_index]
 test_set = digits.data[second_index:]
 
-print("training_set length: ", len(training_set_x))
-print("cross_validation_set_x length: ", len(cross_validation_set_x))
-print("test_set length: ", len(test_set))
+print("training_set length: " + str(len(training_set_x)))
+print("cross_validation_set_x length: " + str(len(cross_validation_set_x)))
+print("test_set length: " + str(len(test_set)))
 
 clf = svm.SVC(gamma=0.001, C=100)
-
-#import numpy
-from sklearn.metrics import accuracy_score
 
 clf.fit(training_set_x, training_set_y)
 cross_validation_predictions = clf.predict(cross_validation_set_x)
 score = accuracy_score(cross_validation_predictions, cross_validation_set_y)
-print("Accuracy Score: ", score) # percent correct
+print("Accuracy Score: " + str(score)) # percent correct
